6de99555d9d6857fafbd1cae95e826a350b68721
benbooi <ben.booi@shopify.com>
2025-04-10 12:08:28 -0700

    Fix: Update deployment configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/catan/'
+  base: '/catan/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 })
